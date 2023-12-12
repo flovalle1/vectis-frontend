0e1cef1e06664b6048bc0e59eaf2f7a12d9b0cb9
Florian Martel <flovalle@LAPTOP-BQ82DNBN>
2023-12-12 14:33:19 +0100

    Dashboard update

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,63 +76,43 @@ const Page = () => (
               orders={[
                 {
                   id: 'f69f88012978187a6c12897f',
-                  ref: 'DEV1049',
-                  amount: 30.5,
+                  ref: 'Einfamilienhaus',
+                  amount: "170.5qm",
                   customer: {
-                    name: 'Ekaterina Tankova'
+                    name: 'Marzellenstraße 5, 72070 Köln'
                   },
                   createdAt: 1555016400000,
-                  status: 'pending'
+                  status: 'besichtigt'
                 },
                 {
                   id: '9eaa1c7dd4433f413c308ce2',
-                  ref: 'DEV1048',
-                  amount: 25.1,
+                  ref: 'Doppelhaushälfte',
+                  amount: "139.1qm",
                   customer: {
-                    name: 'Cao Yu'
+                    name: 'Karlstraße 3, 72072 Tübingen'
                   },
                   createdAt: 1555016400000,
-                  status: 'delivered'
+                  status: 'finanzierbar'
                 },
                 {
                   id: '01a5230c811bd04996ce7c13',
-                  ref: 'DEV1047',
-                  amount: 10.99,
+                  ref: 'Einzimmerwohnung',
+                  amount: "17qm",
                   customer: {
-                    name: 'Alexa Richardson'
+                    name: 'Wilhelmsstraße 4, 72072 Tübingen'
                   },
                   createdAt: 1554930000000,
-                  status: 'refunded'
+                  status: 'abgelehnt'
                 },
                 {
                   id: '1f4e1bd0a87cea23cdb83d18',
-                  ref: 'DEV1046',
-                  amount: 96.43,
+                  ref: 'Pärchen-Wohnung',
+                  amount: "65qm",
                   customer: {
-                    name: 'Anje Keizer'
+                    name: 'Niselweg 5, 72622 Nürtingen'
                   },
                   createdAt: 1554757200000,
-                  status: 'pending'
-                },
-                {
-                  id: '9f974f239d29ede969367103',
-                  ref: 'DEV1045',
-                  amount: 32.54,
-                  customer: {
-                    name: 'Clarke Gillebert'
-                  },
-                  createdAt: 1554670800000,
-                  status: 'delivered'
-                },
-                {
-                  id: 'ffc83c1560ec2f66a1c05596',
-                  ref: 'DEV1044',
-                  amount: 16.76,
-                  customer: {
-                    name: 'Adam Denisov'
-                  },
-                  createdAt: 1554670800000,
-                  status: 'delivered'
+                  status: 'finanzierbar'
                 }
               ]}
               sx={{ height: '100%' }}
--- src/sections/overview/overview-latest-orders.js
+++ src/sections/overview/overview-latest-orders.js
@@ -8,6 +8,8 @@ import {
   CardActions,
   CardHeader,
   Divider,
+  Grid,
+  Icon,
   SvgIcon,
   Table,
   TableBody,
@@ -17,11 +19,14 @@ import {
 } from '@mui/material';
 import { Scrollbar } from 'src/components/scrollbar';
 import { SeverityPill } from 'src/components/severity-pill';
+import DocumentArrowDownIcon from '@heroicons/react/24/solid/DocumentArrowDownIcon';
+import IconButton from '@mui/material/IconButton';
+import ArchiveBoxXMarkIcon from '@heroicons/react/24/solid/ArchiveBoxXMarkIcon';
 
 const statusMap = {
-  pending: 'warning',
-  delivered: 'success',
-  refunded: 'error'
+  'besichtigt': 'warning',
+  'finanzierbar': 'success',
+  'abgelehnt': 'error'
 };
 
 export const OverviewLatestOrders = (props) => {
@@ -29,23 +34,29 @@ export const OverviewLatestOrders = (props) => {
 
   return (
     <Card sx={sx}>
-      <CardHeader title="Latest Orders" />
+      <CardHeader title="Immobilienprojekte" />
       <Scrollbar sx={{ flexGrow: 1 }}>
         <Box sx={{ minWidth: 800 }}>
           <Table>
             <TableHead>
               <TableRow>
                 <TableCell>
-                  Order
+                  Immobilie
                 </TableCell>
                 <TableCell>
-                  Customer
+                  Adresse
                 </TableCell>
                 <TableCell sortDirection="desc">
-                  Date
+                  Wohnfläche
+                </TableCell>
+                <TableCell sortDirection="desc">
+                  Kaufpreis
                 </TableCell>
                 <TableCell>
                   Status
+                </TableCell>
+                <TableCell>
+                  Aktionen
                 </TableCell>
               </TableRow>
             </TableHead>
@@ -65,12 +76,36 @@ export const OverviewLatestOrders = (props) => {
                       {order.customer.name}
                     </TableCell>
                     <TableCell>
-                      {createdAt}
+                      {order.amount}
+                    </TableCell>
+                    <TableCell>
+                      {order.amount}
                     </TableCell>
                     <TableCell>
                       <SeverityPill color={statusMap[order.status]}>
                         {order.status}
                       </SeverityPill>
+                    </TableCell>
+                    <TableCell>
+                    <Grid container spacing={0.5}>
+                        <Grid item xs = {6}>
+                        {order.status === 'finanzierbar' && (
+                              <IconButton edge="end" >
+                                <SvgIcon>
+                                  <DocumentArrowDownIcon />
+                                </SvgIcon>
+                              </IconButton>
+                            )}
+                        {!order.status && <div style={{ width: 48, height: 48 }}></div>}
+                        </Grid>
+                        <Grid item xs = {6}>
+                              <IconButton edge="end" >
+                                <SvgIcon style={{ ':hover': { backgroundColor: 'red' } }}>
+                                  <ArchiveBoxXMarkIcon />
+                                </SvgIcon>
+                              </IconButton>
+                        </Grid>
+                      </Grid>
                     </TableCell>
                   </TableRow>
                 );
@@ -91,7 +126,7 @@ export const OverviewLatestOrders = (props) => {
           size="small"
           variant="text"
         >
-          View all
+          Alle ansehen
         </Button>
       </CardActions>
     </Card>
